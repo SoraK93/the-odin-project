0945c3f669a8eb353a24e12f530622d36f1bcc87
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-10-05 04:41:00 +0530

    Learning setup, update, and rules of react

---
+++ react-course/setting-up-react-environment/my-first-react-app/src/testing.jsx
@@ -0,0 +1,23 @@
+{
+  /* <h1>Test title</h1>
+<svg>
+  <circle cx="25" cy="75" r="20" stroke="green" stroke-width="2" />
+</svg>
+<form>
+  <input type="text">
+</form> */
+}
+
+export default function Testing() {
+  return (
+    <div>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" name="test" />
+      </form>
+    </div>
+  );
+}
