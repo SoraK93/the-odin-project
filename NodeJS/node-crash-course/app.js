21c558ca265191e86258a9d4620e9139a7e22144
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-10-17 08:28:22 +0530

    Refactor file structure and code, Add mongodb and schema

--- NodeJS/node-crash-course/app.js
+++ NodeJS/node-crash-course/app.js
@@ -1,32 +1,50 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./public/models/blog.js");
 
 // express app
 const app = express();
 
-// register view engine
-app.set("view engine", "ejs");
-// set path for view engine to look for html resources => app.set("views", "/resource-path")
+// connection to mongodb
+const dbURI =
+  "mongodb+srv://:@cluster0.tedzxy3.mongodb.net/note-tuts?retryWrites=true&w=majority&appName=Cluster0";
 
-// listen for requests
-app.listen(3000);
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    // listen for requests
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
+
+// register view engine
+// set path for view engine to look for html resources => app.set("views", "/resource-path")
+app.set("view engine", "ejs");
+
+// middleware
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 // routing - get request
 app.get("/", (req, res) => {
-  const blogs = [
-    {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur."  },
-    {title: "Mario find stars", snippet: "Lorem ipsum dolor sit amet consectetur."  },
-    {title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur."  }
-  ]
-  res.render("index", { title: "Home", blogs });
-  //   res.sendFile("./views/index.html", { root: __dirname });
-  //   res.send("<p>Homepage</p>");
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
-  // res.sendFile("./views/about.html", { root: __dirname });
-  //   res.send("<p>Homepage</p>");
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result})
+    })
+    .catch((err) => console.log(err))
+  // Older way of liking html
+  // res.sendFile("./views/index.html", { root: __dirname });
+  // res.send("<p>Homepage</p>");
 });
 
 app.get("/blog/create", (req, res) => {
@@ -38,10 +56,55 @@ app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
+// // mongoose and mongo sandbox routes
+// // adds data to db
+// app.get("/add-blog", (req, res) => {
+//   const blog = new Blog({
+//     title: "new blog 2",
+//     snippet: "about my new blog",
+//     body: "more about my new blog",
+//   });
+
+//   blog
+//     .save()
+//     .then((result) => res.send(result))
+//     .catch((err) => console.log(err));
+// });
+
+// // get all data from db
+// app.get("/all-blogs", (req, res) => {
+//   Blog.find()
+//     .then((result) => res.send(result))
+//     .catch((err) => console.log(err))
+// })
+
+// // get specific data from db
+// app.get("/single-blog", (req, res) => {
+//   Blog.findById("68f1ab13c50696077db3b0ce")
+//     .then((result) => res.send(result))
+//     .catch((err) => console.log(err))
+// })
+
 // 404 page
 app.use((req, res) => {
   // this .use() executes for every URL, thus none of the code below will excute
   // !!!!ALWAYS KEEP THIS AT THE BOTTOM OF THE SCRIPT!!!!
   res.status(404).render("404", { title: "Not Found" });
-  // res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
+
+
+// const blogs = [
+//   {
+//     title: "Yoshi finds eggs",
+//     snippet: "Lorem ipsum dolor sit amet consectetur.",
+//   },
+//   {
+//     title: "Mario find stars",
+//     snippet: "Lorem ipsum dolor sit amet consectetur.",
+//   },
+//   {
+//     title: "How to defeat bowser",
+//     snippet: "Lorem ipsum dolor sit amet consectetur.",
+//   },
+// ];
+// res.render("index", { title: "Home", blogs });
