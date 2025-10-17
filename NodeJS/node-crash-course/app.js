4365764e0f4863bcb6d14aa46a436246445d9d12
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-10-17 06:18:56 +0530

    Complete: View Engines Class

--- NodeJS/node-crash-course/app.js
+++ NodeJS/node-crash-course/app.js
@@ -13,20 +13,25 @@ app.listen(3000);
 
 // routing - get request
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur."  },
+    {title: "Mario find stars", snippet: "Lorem ipsum dolor sit amet consectetur."  },
+    {title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur."  }
+  ]
+  res.render("index", { title: "Home", blogs });
   //   res.sendFile("./views/index.html", { root: __dirname });
   //   res.send("<p>Homepage</p>");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about")
+  res.render("about", { title: "About" });
   // res.sendFile("./views/about.html", { root: __dirname });
   //   res.send("<p>Homepage</p>");
 });
 
 app.get("/blog/create", (req, res) => {
-  res.render("create");
-})
+  res.render("create", { title: "Create a new blog" });
+});
 
 // redirects
 app.get("/about-us", (req, res) => {
@@ -37,6 +42,6 @@ app.get("/about-us", (req, res) => {
 app.use((req, res) => {
   // this .use() executes for every URL, thus none of the code below will excute
   // !!!!ALWAYS KEEP THIS AT THE BOTTOM OF THE SCRIPT!!!!
-  res.status(404).render("404")
+  res.status(404).render("404", { title: "Not Found" });
   // res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
